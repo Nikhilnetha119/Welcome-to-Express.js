974e596de072270107fe6f052ebc6f81b108ac47
Nikhil kumar <89766775+Nikhilnetha119@users.noreply.github.com>
2024-04-01 18:44:48 +0530

    Parsedbody

--- project1.js
+++ project1.js
@@ -1,16 +1,27 @@
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const project = express();
 
-project.use((req, res, next) => {
-    console.log('In the middleware!')
-    next();
+project.use(bodyParser.urlencoded({extended : false}))
+
+project.get('/add-product', (req, res, next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='product'><input type='number' name='size'><button type='submit'>Add product</button></form>")
+});
+
+project.post('/product',( req, res, next) => {
+    const product = req.body.product;
+    const size = req.body.size;
+
+    console.log('product:', product)
+    console.log('size:', size)
+    console.log(req.body);
+    res.redirect('/');
 })
 
-project.use((req, res, next) => {
-    console.log('out the middleware!')
-    res.send('<h1>Hello from Express.js!!!<h1>');
+project.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express.js!!<h1>');
 })
 
 project.listen(3000);
